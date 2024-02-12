69c232aa7aaa21f0291faade3e3806134e9af469
George <ndungugeorge910@gmail.com>
2024-02-12 20:49:45 +0300

    updates

--- base.py
+++ base.py
@@ -1,24 +1,71 @@
 class Base:
     __nb_objects = 0
-    def  __init__(self,id = None):
-        if id != None:
+    
+    def __init__(self, id=None):
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
-b1 = Base()
-print(b1.id)
+    @property
+    def width(self):
+        return self.__width
 
-b2 = Base()
-print(b2.id)
+    @width.setter
+    def width(self, value):
+        self.validator("width", value)
+        self.__width = value
 
-b3 = Base()
-print(b3.id)
+    @property
+    def height(self):
+        return self.__height
 
-b4 = Base(12)
-print(b4.id)
+    @height.setter
+    def height(self, value):
+        self.validator("height", value)
+        self.__height = value
 
-b5 = Base()
-print(b5.id)
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.validator("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.validator("y", value)
+        self.__y = value
+
+    def validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+    def area(self):
+        return self.height * self.width
+if __name__ == "__main__":
+
+    r1 = Rectangle(3, 2)
+    print(r1.area())
+
+    r2 = Rectangle(2, 10)
+    print(r2.area())
+
+    r3 = Rectangle(8, 7, 0, 0, 12)
+    print(r3.area())
